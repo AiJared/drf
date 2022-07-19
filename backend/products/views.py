504b366a38ebf119af3e22e1d89d7174da9e3aa7
Kerriea-star <ojared511@gmail.com>
2022-07-19 20:58:51 +0300

    permissions and authentications

--- backend/products/views.py
+++ backend/products/views.py
@@ -12,7 +12,7 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     authentication_classes = [authentication.SessionAuthentication]
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.DjangoModelPermissions]
 
     def perfom_create(self, serializer):
         # serializer.save(user=self.request.user)
