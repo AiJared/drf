3bc5994290feca8602486e235ab2f6a3a2613a81
Kerriea-star <ojared511@gmail.com>
2022-07-20 10:57:38 +0300

    viewsets and routers

---
+++ backend/cfehome/routers.py
@@ -0,0 +1,9 @@
+from django.db import router
+from rest_framework.routers import DefaultRouter
+
+
+from products.viewsets import ProductGenericViewSet
+
+router = DefaultRouter()
+router.register('products', ProductGenericViewSet, basename="products")
+urlpatterns = router.urls
