2f1da300d217cb28895fa2c2ec8bfc872fd725f0
Kerriea-star <ojared511@gmail.com>
2022-07-20 21:03:29 +0300

    custom validation

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,7 +1,7 @@
-from wsgiref import validate
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 from products.models import Product
+from .import validators
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
@@ -10,18 +10,30 @@ class ProductSerializer(serializers.ModelSerializer):
         view_name="product-detail",
         lookup_field = 'pk'
     )
+    title = serializers.CharField(validators=[validators.validate_title_no_hello,
+                                    validators.unique_product_title])
+    # email = serializers.CharField(source='user.email', read_only=True)
     class Meta:
         model = Product
         fields = [
             "url",
             "edit_url",
             "pk",
+            #"name",
             "title",
             "content",
             "price",
             "sale_price",
             "my_discount",
         ]
+
+    # def validate_title(self, value):
+    #     request = self.context.get('request')
+    #     user = request.user
+    #     qs = Product.objects.filter(user=user, title__iexact=value)
+    #     if qs.exists():
+    #         raise serializers.ValidationError(f"{value} is already a product name")
+    #     return value
 
     # def create(self, validated_data):
     #     #return Product.objects.create(**validated_data)
